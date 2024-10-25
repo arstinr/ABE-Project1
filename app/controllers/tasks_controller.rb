9a9bc43faa54b9c52dd0931579c648d92cb068aa
Arstin Reano <arstingabrelreano@gmail.com>
2024-10-25 11:54:05 +0800

    Add ,edit ,delete tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,42 +8,51 @@ class TasksController < ApplicationController
     end
 
     def new
-        @task = Task.new
+        if params[:category_id]
+            @category = Category.find(params[:category_id])
+            @task = @category.tasks.build
+        else
+            @task = Task.new
+        end
     end
 
     def create
         @task = Task.new(task_params)
 
         if @task.save
-            redirect_to @task
+            redirect_to category_path(@task.category), notice: "Task was successfully created."
         else
-            render :new, status: unprocessable_entity
+            @category = @task.category || Category.find(params[:category_id]) if params[:category_id]
+            render :new, status: :unprocessable_entity
         end
     end
 
     def edit
         @task = Task.find(params[:id])
+        @category = @task.category
     end
 
     def update
         @task = Task.find(params[:id])
+        @category = @task.category
 
         if @task.update(task_params)
-            redirect_to @task, notice: "Task updated!"
+            redirect_to category_task_path(@task), notice: "Task updated!"
         else
-            render :edit, status: unprocessable_entity
+            render :edit, status: :unprocessable_entity
         end
     end
 
     def destroy
         @task = Task.find(params[:id])
+        category_id = @task.category_id
         @task.destroy
 
-        redirect_to tasks_path
+        redirect_to category_path(category_id), notice: "Task was successfully deleted."
     end
 
     private
     def task_params
-        params.require(:task).permit(:name, :details, :status, :due_date)
+        params.require(:task).permit(:name, :details, :status, :due_date, :category_id)
     end
 end
